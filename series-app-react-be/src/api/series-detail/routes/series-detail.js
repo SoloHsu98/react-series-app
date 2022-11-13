a0d1d0e41ab6d4af9349233a33da3453cf129754
Solo Hsu <53265779+SoloHsu-coder@users.noreply.github.com>
2022-11-13 21:52:04 +0630

    Initial commit

---
+++ series-app-react-be/src/api/series-detail/routes/series-detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * series-detail router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::series-detail.series-detail');
---
+++ series-app-react-be/src/api/series-detail/services/series-detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * series-detail service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::series-detail.series-detail');
